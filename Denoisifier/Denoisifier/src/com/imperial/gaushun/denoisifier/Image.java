ff523b3ada16fe1a9c0b127be3b8e4dbf5e61fdc
Gautam K <gautampk@linux.com>
2014-05-30 13:14:38 +0100

    Added local mean filter

--- Denoisifier/Denoisifier/src/com/imperial/gaushun/denoisifier/Image.java
+++ Denoisifier/Denoisifier/src/com/imperial/gaushun/denoisifier/Image.java
@@ -9,20 +9,24 @@ import javax.imageio.*;
 
 //Currently loads an image (line 13) and then copies it as a PNG (line 14).
 //Now generates noise and applies it to the image (demonstrate pixel-by-pixel editing).
+//Need to supply command line option now:
+//**> cd /path/to/download/folder
+//**> cd bin
+//**> java com.imperial.gaushun.denoisifier.Image "/path/to/image"
 
 public class Image {
 	public static void main(String[] args) {
 		try {
 			BufferedImage img = ImageIO.read(new File(args[0]));
-			System.out.println("Loading the image at " + args[0] + "\\...");
+			System.out.println("Loading the image at " + args[0] + " ...");
 		    Random rnd = new Random(System.currentTimeMillis());
 		    
 		    int wImg = img.getWidth();
 		    int hImg = img.getHeight();
 		    int x,y,RGB,col;
 		    
-		    System.out.println("Applying noise...");
-		    for(int i=0;i<=(wImg*hImg)/4;i++){
+		    System.out.println("Applying noise ...");
+		    for(int i=0;i<=(wImg*hImg)/1.2;i++){
 			    x = rnd.nextInt(wImg);
 			    y = rnd.nextInt(hImg);
 			    col = rnd.nextInt(256);
@@ -30,8 +34,50 @@ public class Image {
 			    img.setRGB(x,y,RGB);
 		    }
 		    
-		    System.out.println("Saving modified image to " + args[0]+".copy.png" + "\\...");
-		    ImageIO.write(img, "png", new File(args[0]+".copy.png"));
+		    System.out.println("Saving noised image to " + args[0]+".noise.png" + " ...");
+		    ImageIO.write(img, "png", new File(args[0]+".noise.png"));
+		    
+		    for(int i=1;i<wImg;i+=3){
+		    	for(int j=1;j<hImg;j+=3){
+		    		if(i>=wImg-1 || j>=hImg-1){
+		    			break;
+		    		}
+		    		int[] pxlRGB = {0,0,0,0,0,0,0,0,0};
+		    		pxlRGB[0] = img.getRGB(i-1,j+1);
+		    		pxlRGB[0] = img.getRGB(i,j+1);
+		    		pxlRGB[0] = img.getRGB(i+1,j+1);
+		    		pxlRGB[0] = img.getRGB(i-1,j);
+		    		pxlRGB[0] = img.getRGB(i,j);
+		    		pxlRGB[0] = img.getRGB(i+1,j);
+		    		pxlRGB[0] = img.getRGB(i-1,j-1);
+		    		pxlRGB[0] = img.getRGB(i,j-1);
+		    		pxlRGB[0] = img.getRGB(i+1,j-1);
+		    		
+		    		int[] meanRGB = {0,0,0,0};
+		    		for(int k=0;k<=8;k++){
+		    			meanRGB[0] += new Color(pxlRGB[k]).getRed();
+		    			meanRGB[1] += new Color(pxlRGB[k]).getGreen();
+		    			meanRGB[2] += new Color(pxlRGB[k]).getBlue();
+		    		}
+		    		meanRGB[0] /= 9;
+		    		meanRGB[1] /= 9;
+		    		meanRGB[2] /= 9;
+		    		meanRGB[3] = new Color(meanRGB[0], meanRGB[1], meanRGB[2]).getRGB();
+		    		
+		    		img.setRGB(i-1,j-1,meanRGB[3]);
+		    		img.setRGB(i,j-1,meanRGB[3]);
+		    		img.setRGB(i+1,j-1,meanRGB[3]);
+		    		img.setRGB(i-1,j,meanRGB[3]);
+		    		img.setRGB(i,j,meanRGB[3]);
+		    		img.setRGB(i+1,j,meanRGB[3]);
+		    		img.setRGB(i-1,j+1,meanRGB[3]);
+		    		img.setRGB(i,j+1,meanRGB[3]);
+		    		img.setRGB(i+1,j+1,meanRGB[3]);
+		    	}
+		    }
+		    
+		    System.out.println("Saving denoised image to " + args[0]+".clean.png" + " ...");
+		    ImageIO.write(img, "png", new File(args[0]+".clean.png"));
 		} catch (IOException e) {
 			System.out.println("Input/Output Error. Check filepaths.");
 		}
