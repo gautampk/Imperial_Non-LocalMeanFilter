436d43482eb244a24c96e85946ca10b46e06fd9d
Gautam K <gautampk@linux.com>
2014-05-30 12:03:28 +0100

    Adding noise

--- Denoisifier/Denoisifier/src/com/imperial/gaushun/denoisifier/Image.java
+++ Denoisifier/Denoisifier/src/com/imperial/gaushun/denoisifier/Image.java
@@ -1,16 +1,33 @@
 package com.imperial.gaushun.denoisifier;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import javax.imageio.ImageIO;
+import java.util.Random;
+
+import javax.imageio.*;
 
 //Currently loads an image (line 13) and then copies it as a PNG (line 14).
+//Now generates noise and applies it to the image (demonstrate pixel-by-pixel editing).
 
 public class Image {
 	public static void main(String[] args) {
-		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(new File("C:\\Users\\Gautam\\Desktop\\test.jpg"));
+			BufferedImage img = ImageIO.read(new File("C:\\Users\\Gautam\\Desktop\\test.jpg"));
+		    Random rnd = new Random();
+		    
+		    int wImg = img.getWidth();
+		    int hImg = img.getHeight();
+		    int x,y,RGB,col;
+		    
+		    for(int i=0;i<=(wImg*hImg)/4;i++){
+			    x = rnd.nextInt(wImg);
+			    y = rnd.nextInt(hImg);
+			    col = rnd.nextInt(256);
+			    RGB = new Color(col,col,col).getRGB();
+			    img.setRGB(x,y,RGB);
+		    }
+		    
 		    ImageIO.write(img, "png", new File("C:\\Users\\Gautam\\Desktop\\test_copy.png"));
 		} catch (IOException e) {}
 	}
